6c64b43ac27d1b56fdb13915d6df69005da251d6
matteo folin <matteo@00-25-00-3e-2b-93.dummy.porta.siemens.net>
2010-05-23 11:21:40 +0200

    menù vendite-ordini-report con submenù funzionanti

--- apps/fweb/resources/main_page.js
+++ apps/fweb/resources/main_page.js
@@ -4,19 +4,6 @@
 // ==========================================================================
 /*globals Fweb */
 
-	var submenuitems = [
-	{ title: 'sub uno 1', isEnabled: YES},
-	{ title: 'sub due 2', isEnabled: YES}
-	]; 
-	var menuitems = [
-	{ title: '1 Item with sub', isEnabled: YES, subMenu: submenuitems, keyEquivalent: 'ctrl_shift_n', target: 'Fweb' },
-	{ title: '2 Checked Item', isEnabled: YES, isChecked: YES, keyEquivalent: 'ctrl_a', checkbox: YES },
-	{ title: '3 Disabled Item', isEnabled: NO, keyEquivalent: 'backspace' },
-	{ isSeparator: YES },
-	{ title: '4 Item with Icon', isEnabled: YES, icon: 'email-icon', keyEquivalent: 'ctrl_m' },
-	{ title: '5 Item with Icon', isEnabled: YES, icon: 'folder', keyEquivalent: 'ctrl_p' }
-	];
-
 // This page describes the main user interface for your application.  
 Fweb.mainPage = SC.Page.design({
 
@@ -24,31 +11,11 @@ Fweb.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'MainMenuView textButton'.w(),
+    childViews: 'MainMenuView'.w(),
 
 		MainMenuView: Fweb.MainMenuView.design({
 			layout: { top: 0, left: 0, height: 100 },
-		}),
-			
-		textButton: SC.ButtonView.design(SCUI.DropDown, {
-      layout: { centerY: 0, centerX: 0, top: 100, height: 40, width: 100 },
-			title: 'Bottone 1',
-      toolTip: 'text button with dropdown',
-      dropDown: SC.MenuPane.design({
-        layout: { width: 170, height: 0 },
-        itemTitleKey: 'title',
-        itemIconKey: 'icon',
-        itemTargetKey: 'target',
-        itemActionKey: 'action',
-        itemSeparatorKey: 'isSeparator',
-        itemIsEnabledKey: 'isEnabled',
-        itemCheckboxKey: 'checkbox',
-				itemSubMenuKey: 'subMenu',
-  			contentView: SC.View.design({}),
-				// isModal: NO,
-  			items: menuitems //[{title: '1', isEnabled: YES}, {title: '2'}] //
-  		})
-    }),
+		})
 
 	})
 	
--- apps/fweb/views/main_menu.js
+++ apps/fweb/views/main_menu.js
@@ -16,13 +16,54 @@ Fweb.MainMenuView = SC.View.extend(
 	createChildViews: function(){
     var view, childViews = [];
 		
-		/* menù list objects */
-		var submenuitems = [
-			{ title: 'sub uno'},
-			{ title: 'sub due'}
-		]; 
-		var menuitems = [
-			{ title: 'Item with sub', keyEquivalent: 'ctrl_shift_n', subMenu: submenuitems, target: 'Fweb' },
+		/* menù vendite */
+		var subMenuOrdiniReport = [
+			{ title: 'sub sub uno 1', isEnabled: YES},
+			{ title: 'sub sub due 2', isEnabled: YES}
+		];
+		var subMenuOrdini = [
+			{ title: 'nuovo', keyEquivalent: 'ctrl_shift_n', icon: 'new-icon' },
+			{ title: 'ricerca', icon: 'search-icon' },
+			{ title: 'report', icon: 'report-icon', subMenu: subMenuOrdiniReport },
+			{ isSeparator: YES },
+			{ title: '990 - Alpeat' },
+			{ title: '989 - L.P.T.' },
+			{ title: '988 - Vant Srl' },
+			{ title: '987 - Sb - lavorazioni Snc' },
+			{ title: '986 - Fincantieri Spa' },
+			{ title: 'vedi tutti ...' }
+		];
+		var menuVendite = [
+			{ title: 'offerte'},
+			{ title: 'ordini', subMenu: subMenuOrdini, target: 'Fweb' },
+			{ title: 'ddt' },
+			{ title: 'fatture' },
+			{ isSeparator: YES },
+			{ title: 'Selected Item', keyEquivalent: 'backspace' },
+			{ title: 'Item with Icon', isChecked: YES, icon: 'email-icon', keyEquivalent: 'ctrl_m' },
+			{ title: 'Item with Icon', checkbox: YES, icon: 'folder', keyEquivalent: 'ctrl_p' }
+		];
+		/* menù acquisti */
+		var menuAcquisti = [
+			{ title: 'Item with sub', keyEquivalent: 'ctrl_shift_n', target: 'Fweb' },
+			{ title: 'Checked Item', isChecked: YES, keyEquivalent: 'ctrl_a', checkbox: YES },
+			{ title: 'Selected Item', keyEquivalent: 'backspace' },
+			{ isSeparator: YES },
+			{ title: 'Item with Icon', icon: 'email-icon', keyEquivalent: 'ctrl_m' },
+			{ title: 'Item with Icon', icon: 'folder', keyEquivalent: 'ctrl_p' }
+		];
+		/* menù amministrazione */
+		var menuAmministrazione = [
+			{ title: 'Item with sub', keyEquivalent: 'ctrl_shift_n', target: 'Fweb' },
+			{ title: 'Checked Item', isChecked: YES, keyEquivalent: 'ctrl_a', checkbox: YES },
+			{ title: 'Selected Item', keyEquivalent: 'backspace' },
+			{ isSeparator: YES },
+			{ title: 'Item with Icon', icon: 'email-icon', keyEquivalent: 'ctrl_m' },
+			{ title: 'Item with Icon', icon: 'folder', keyEquivalent: 'ctrl_p' }
+		];
+		/* menù magazzino */
+		var menuMagazzino = [
+			{ title: 'Item with sub', keyEquivalent: 'ctrl_shift_n', target: 'Fweb' },
 			{ title: 'Checked Item', isChecked: YES, keyEquivalent: 'ctrl_a', checkbox: YES },
 			{ title: 'Selected Item', keyEquivalent: 'backspace' },
 			{ isSeparator: YES },
@@ -35,7 +76,7 @@ Fweb.MainMenuView = SC.View.extend(
       SC.ToolbarView.design({
         layout: {left: 0, right: 0, top: 0, height: 44},
 				classNames: ['topbar'],
-        childViews: 'logo vendite'.w(),
+        childViews: 'logo vendite acquisti amministrazione magazzino endButton'.w(),
 
 				logo: SC.LabelView.design({
 					layout: { top: 2, left: 26, height: 40, width: 147 },
@@ -58,9 +99,75 @@ Fweb.MainMenuView = SC.View.extend(
 	          itemCheckboxKey: 'checkbox',
 	    			contentView: SC.View.design({}),
 						// isModal: NO,
-	    			items: menuitems
+	    			items: menuVendite
 	    		})
-      	})
+      	}),
+
+				acquisti: SC.ButtonView.design(SCUI.DropDown, {
+	        layout: { centerY: 0, left: 300, height: 40, width: 100 },
+					title: 'Acquisti',
+	        classNames: ['bar-button'],
+	        toolTip: 'Menù acquisti: offerte, ordini...', 
+					dropDown: SC.MenuPane.design({
+		        layout: { width: 170, height: 0 },
+	          itemTitleKey: 'title',
+	          itemIconKey: 'icon',
+	          itemTargetKey: 'target',
+	          itemActionKey: 'action',
+	          itemSeparatorKey: 'isSeparator',
+	          itemIsEnabledKey: 'isEnabled',
+	          itemCheckboxKey: 'checkbox',
+	    			contentView: SC.View.design({}),
+						// isModal: NO,
+	    			items: menuAcquisti
+	    		})
+      	}),
+
+				amministrazione: SC.ButtonView.design(SCUI.DropDown, {
+	        layout: { centerY: 0, left: 400, height: 40, width: 100 },
+					title: 'Amministrazione',
+	        classNames: ['bar-button'],
+	        toolTip: 'Menù amministrazione: contabilità, anagrafica...', 
+					dropDown: SC.MenuPane.design({
+		        layout: { width: 170, height: 0 },
+	          itemTitleKey: 'title',
+	          itemIconKey: 'icon',
+	          itemTargetKey: 'target',
+	          itemActionKey: 'action',
+	          itemSeparatorKey: 'isSeparator',
+	          itemIsEnabledKey: 'isEnabled',
+	          itemCheckboxKey: 'checkbox',
+	    			contentView: SC.View.design({}),
+						// isModal: NO,
+	    			items: menuAmministrazione
+	    		})
+      	}),
+
+				magazzino: SC.ButtonView.design(SCUI.DropDown, {
+	        layout: { centerY: 0, left: 500, height: 40, width: 100 },
+					title: 'Magazzino',
+	        classNames: ['bar-button'],
+	        toolTip: 'Menù magazzino: materiali, movimenti...', 
+					dropDown: SC.MenuPane.design({
+		        layout: { width: 170, height: 0 },
+	          itemTitleKey: 'title',
+	          itemIconKey: 'icon',
+	          itemTargetKey: 'target',
+	          itemActionKey: 'action',
+	          itemSeparatorKey: 'isSeparator',
+	          itemIsEnabledKey: 'isEnabled',
+	          itemCheckboxKey: 'checkbox',
+	    			contentView: SC.View.design({}),
+						// isModal: NO,
+	    			items: menuMagazzino
+	    		})
+      	}),
+
+				endButton: SC.LabelView.design({
+	        layout: { centerY: 0, left: 601, height: 40, width: 0 },
+	        classNames: ['bar-button']
+	      })
+	
       })
     );
     childViews.push(view);
