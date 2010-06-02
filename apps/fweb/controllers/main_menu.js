4ee9e9857498f24dc21c301b82e39b5e2aeb012f
matteo folin <matteo@computer-di-matteo-folin-2.local>
2010-06-02 17:24:37 +0200

    added controller mainMenu

---
+++ apps/fweb/controllers/main_menu.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// Project:   Fweb.mainMenuController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Fweb */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Fweb.mainMenuController = SC.ObjectController.create(
+/** @scope Fweb.mainMenuController.prototype */ {
+
+	print: function() {
+		window.print();
+	},
+
+}) ;
