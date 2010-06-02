c840bca57d41c1b41add69942f8e1ec258985f59
matteo folin <matteo@computer-di-matteo-folin-2.local>
2010-06-03 00:33:26 +0200

    added recent model

---
+++ apps/fweb/tests/models/recent.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Fweb.Recent Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Fweb module test ok equals same stop start */
+
+module("Fweb.Recent");
+
+// TODO: Replace with real unit test for Recent
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
