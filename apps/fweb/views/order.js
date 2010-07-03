677e91c0c112c87d6c8100b8475ef38115958918
matteo folin <matteo@computer-di-matteo-folin-2.local>
2010-07-03 15:55:24 +0200

    added some order fields

--- apps/fweb/views/order.js
+++ apps/fweb/views/order.js
@@ -21,37 +21,125 @@ Fweb.OrderView = SC.View.extend(
 			SC.View.design({
 		    layout: { left: 0, right: 0, top: 0, height: 50 },
 				classNames: ['ord-head'],
-				childViews: 'numL num dateL date ragSocCliL ragSocCli'.w(),
-							
+				childViews: 'num date ragSocCli clientRef dateClientRef dateDelivery ordStatus ordType ordDesc operator numL dateL ragSocCliL clientRefL dateClientRefL dateDeliveryL ordStatusL ordTypeL ordDescL operatorL'.w(),
+				// general data - Label
 				numL: SC.LabelView.design({
 					layout: { left: 0, top: 2, height: 16, width: 60 },
 					classNames: ['ord-field-label'],
 					value: 'numero',
 				}),
-				num: SC.TextFieldView.design({
-					layout: { left: 0, top: 20, height: 20, width: 60 },
-					valueBinding: 'Fweb.orderController.nestedOrder.num',
-				}),
-				
 				dateL: SC.LabelView.design({
 					layout: { left: 70, top: 2, height: 16, width: 80 },
 					classNames: ['ord-field-label'],
 					value: 'data',
 				}),
-				date: SC.TextFieldView.design({
-					layout: { left: 70, top: 20, height: 20, width: 80 },
-					valueBinding: 'Fweb.orderController.nestedOrder.formattedDate',
-				}),
-				
 				ragSocCliL: SC.LabelView.design({
 					layout: { left: 160, top: 2, height: 16, width: 150 },
 					classNames: ['ord-field-label'],
 					value: 'cliente',
 				}),
+				clientRefL: SC.LabelView.design({
+					layout: { left: 320, top: 2, height: 20, width: 100 },
+					classNames: ['ord-field-label'],
+					value: 'rif.cli',
+				}),
+				dateClientRefL: SC.LabelView.design({
+					layout: { left: 430, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'data rif.cli.',
+				}),
+				dateDeliveryL: SC.LabelView.design({
+					layout: { left: 520, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'data sped.',
+				}),
+				ordStatusL: SC.LabelView.design({
+					layout: { left: 610, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'S',
+				}),
+				ordTypeL: SC.LabelView.design({
+					layout: { left: 640, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'tipo',
+				}),
+				ordDescL: SC.LabelView.design({
+					layout: { left: 690, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'descrizone',
+				}),
+				operatorL: SC.LabelView.design({
+					layout: { left: 800, top: 2, height: 20, width: 80 },
+					classNames: ['ord-field-label'],
+					value: 'operatore',
+				}),
+				// general data - Field
+				num: SC.TextFieldView.design({
+					layout: { left: 0, top: 20, height: 20, width: 60 },
+					valueBinding: 'Fweb.orderController.nestedOrder.num',
+				}),
+				date: SC.TextFieldView.design({
+					layout: { left: 70, top: 20, height: 20, width: 80 },
+					valueBinding: 'Fweb.orderController.nestedOrder.formattedDate',
+				}),
 				ragSocCli: SC.TextFieldView.design({
 					layout: { left: 160, top: 20, height: 20, width: 150 },
 					valueBinding: 'Fweb.orderController.nestedOrder.ragSocCli',
 				}),
+				clientRef: SC.TextFieldView.design({
+					layout: { left: 320, top: 20, height: 20, width: 100 },
+					valueBinding: 'Fweb.orderController.nestedOrder.clientRef',
+				}),
+				dateClientRef: SC.TextFieldView.design({
+					layout: { left: 430, top: 20, height: 20, width: 80 },
+					valueBinding: SC.Binding.dateTime('%d/%m/%Y').from('Fweb.orderController.nestedOrder.dateClientRef'),
+				}),
+				dateDelivery: SC.TextFieldView.design({
+					layout: { left: 520, top: 20, height: 20, width: 80 },
+					valueBinding: SC.Binding.dateTime('%d/%m/%Y').from('Fweb.orderController.nestedOrder.dateDelivery'),
+				}),
+				ordStatus: SC.TextFieldView.design({
+					layout: { left: 610, top: 20, height: 20, width: 20 },
+					valueBinding: 'Fweb.orderController.nestedOrder.ordStatus',
+				}),
+				ordType: SC.TextFieldView.design({
+					layout: { left: 640, top: 20, height: 20, width: 40 },
+					valueBinding: 'Fweb.orderController.nestedOrder.ordType',
+				}),
+				ordDesc: SC.TextFieldView.design({
+					layout: { left: 690, top: 20, height: 20, width: 100 },
+					valueBinding: 'Fweb.orderController.nestedOrder.ordDesc',
+				}),
+				operator: SC.TextFieldView.design({
+					layout: { left: 800, top: 20, height: 20, width: 50 },
+					valueBinding: 'Fweb.orderController.nestedOrder.operator',
+				}),
+				
+				/*
+				// destination data
+				destination: SC.Record.attr(String),
+				nation: SC.Record.attr(String),
+				address: SC.Record.attr(String),
+				city: SC.Record.attr(String),
+				cap: SC.Record.attr(String),
+				prov: SC.Record.attr(String),
+				tel: SC.Record.attr(String),
+				fax: SC.Record.attr(String),
+				cell: SC.Record.attr(String),
+				// ddt data
+				ddtCausale: SC.Record.attr(String),
+				ddtPorto: SC.Record.attr(String),
+				ddtTrasporto: SC.Record.attr(String),
+				ddtVettore: SC.Record.attr(String),
+				ddtZonaVettore: SC.Record.attr(String),
+				ddtModoTrasporto: SC.Record.attr(String),
+				zone: SC.Record.attr(String),
+				// general data 
+				agent: SC.Record.attr(String),
+				list: SC.Record.attr(String),
+				valute: SC.Record.attr(String),
+				modPag: SC.Record.attr(String),
+				*/
 				
 			})
 		);
